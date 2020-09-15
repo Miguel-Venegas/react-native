6971f87e8cd84fb03008b0c12a83bfbb7ef3854b
Miguel_Venegas <MiguelVenegas@Miguels-MBP.routerlogin.net>
2020-09-15 09:01:37 -0700

    React Native Nucampsite Project 1st week exercises

---
+++ components/MainComponent.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import Directory from './DirectoryComponent';
+import Home from './HomeComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import {View, Platform} from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AboutNavigator = createStackNavigator(
+    {
+        Home: { screen: About }
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Home: { screen: Contact }
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+
+);
+
+
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {screen: HomeNavigator},
+        Directory: {screen: DirectoryNavigator},
+        About: {screen: AboutNavigator},
+        Contact: {screen: ContactNavigator}
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+class Main extends Component {
+    render() {
+        return (
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MainNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
