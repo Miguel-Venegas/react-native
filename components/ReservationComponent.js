a35e2efce01ecaf699fe0d6f3e0e0d4060eecd3f
Miguel_Venegas <MiguelVenegas@Miguels-MBP.routerlogin.net>
2020-09-28 12:54:25 -0700

    Week 4: configure the Expo MailComposer API, along with a RNE Button component,

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -186,35 +186,3 @@ const styles = StyleSheet.create({
 });
 
 export default Reservation;
-
-
-
-// showModal: false,
-
-
-// toggleModal() {
-//     this.setState({showModal: !this.state.showModal});
-// }
-
-
-
-{/* <Modal
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}>
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
-                        <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                        <Text style={styles.modalText}>Date: {this.state.date}</Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>
-                </Modal> */}
